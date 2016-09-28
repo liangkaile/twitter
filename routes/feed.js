e440582cea3e7a0926cda0414657266d2c670ea9
kaliang <kaliang@ebay.com>
2016-09-27 20:05:29 -0700

    add time limit

--- routes/feed.js
+++ routes/feed.js
@@ -5,10 +5,15 @@ var User = require('../models/user.js');
 var config = require('../config');
 var BbPromise = require('bluebird');
 var _ = require('lodash-node');
+var FEED_NUM_LIMIT = config.feedNumLimit || 100;;
+var FEED_DAY_LIMIT = config.feedTimeLimit || 7;
 
-function getTweetsForUser(userId, limit) {
+function getTweetsForUser(userId, numLimit, dayLimit) {
+  var cutoff = new Date();
+  cutoff.setDate(cutoff.getDate() - dayLimit);
+
   return new BbPromise(function (resolve, reject) {
-    Tweet.find({ user: userId }).limit(config.feedLimit).lean().exec(function (err, tweets) {
+    Tweet.find({user: userId, updated_at: {$gte: cutoff}}).limit(FEED_NUM_LIMIT).lean().exec(function (err, tweets) {
       if (err) { 
         reject(err);
       } else {
@@ -33,7 +38,7 @@ function getUserByUserId(userId) {
 router.get('/mine', function(req, res, next) {
   var uid = req.get('uid');
 
-  getTweetsForUser(uid, config.feedLimit).then(function fullfilled(feeds){
+  getTweetsForUser(uid, FEED_NUM_LIMIT, FEED_DAY_LIMIT).then(function fullfilled(feeds){
      res.json(feeds);
   },
   function rejected(error){
@@ -48,7 +53,7 @@ router.get('/', function (req, res, next) {
     function fullfiled(user){
       var tweetPromise = [];
       for (var i = 0; i < user.followIds.length; i++) {
-        tweetPromise.push(getTweetsForUser(user.followIds[i], config.feedLimit));
+        tweetPromise.push(getTweetsForUser(user.followIds[i], FEED_NUM_LIMIT, FEED_DAY_LIMIT));
       }
       BbPromise.all(tweetPromise).then(
         function (tweetLists) {
